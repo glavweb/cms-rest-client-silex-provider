419977f672d1cd9f2f06cfaf0acf88e7d1dfc8ab
nilov <nilov@glavweb.ru>
2016-11-08 11:35:29 +0700

    Implement CmsRestClientServiceProvider, version 0.1.

---
+++ CmsRestClientServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+ * This file is part of the GLAVWEB.cms Rest Client package.
+ *
+ * (c) Andrey Nilov <nilov@glavweb.ru>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Glavweb\CmsRestClientSilexProvider;
+
+use Pimple\Container;
+use Pimple\ServiceProviderInterface;
+
+/**
+ * CmsRestClientServiceProvider
+ *
+ * @package Glavweb\CmsRestClientSilexProvider
+ * @author Andrey Nilov <nilov@glavweb.ru>
+ */
+class CmsRestClientServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Container $app)
+    {
+        $app['cms_rest_client'] = function () use ($app) {
+            return new CmsRestClient(
+                $app['guzzle'],
+                $app['api_base_url'],
+                $app['api_username'],
+                $app['api_password']
+            );
+        };
+    }
+}
